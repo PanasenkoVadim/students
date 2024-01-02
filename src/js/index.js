cfd6e01542b9081b64d4255116af396b4c2a445b
Vadim Panasenko <panasenko@e-krit.ru>
2024-01-02 21:23:35 +0300

    Коммит 2

---
+++ src/js/index.js
@@ -0,0 +1,5 @@
+const { createTable } = require("./table")
+
+document.addEventListener("DOMContentLoaded", function () {
+    createTable()
+})
---
+++ src/js/table.js
@@ -0,0 +1,45 @@
+const students = [
+    {
+        surname: "Иванов",
+        name: "Иван",
+        lastname: "Иванович",
+        date: "03.11.2002",
+        startYear: "2023",
+        faculty: "Фронтенд"
+    },
+    {
+        surname: "Петров",
+        name: "Петр",
+        lastname: "Петрович",
+        date: "03.11.2002",
+        startYear: "2023",
+        faculty: "Фронтенд"
+    },
+    {
+        surname: "Михайлов",
+        name: "Михаил",
+        lastname: "Михайлович",
+        date: "03.11.2002",
+        startYear: "2023",
+        faculty: "Фронтенд"
+    },
+]
+
+
+
+
+const createTableRow = (obj) => {
+    return `<tr>
+    <td>${obj.surname} ${obj.name} ${obj.lastname}</td>
+    <td>${obj.date}</td>
+    <td>${obj.startYear}</td>
+    <td>${obj.faculty}</td>
+</tr>`
+}
+
+export const createTable = () => {
+    const tbody = document.getElementById("tbody")
+    students.forEach(student => {
+        tbody.innerHTML += createTableRow(student)
+    })
+}
